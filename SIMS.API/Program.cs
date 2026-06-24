cccbf81a8fc7d485dde1a1747b43b93b2c95d182
farad47 <50584441+farad47@users.noreply.github.com>
2026-06-24 10:27:26 +0200

    add DI

--- SIMS.API/Program.cs
+++ SIMS.API/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddApplication(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ SIMS.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SIMS.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIMS.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddInfrastructure(configuration);
+            return services;
+        }
+    }
+}
