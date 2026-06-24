f87b9522a65a57a894e5b32391d832cfe963522f
farad47 <50584441+farad47@users.noreply.github.com>
2026-06-24 20:18:51 +0200

    add swagger

--- SIMS.API/Program.cs
+++ SIMS.API/Program.cs
@@ -6,12 +6,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyMarker).Assembly));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SIMS API v1"));
     app.MapOpenApi();
 }
 
