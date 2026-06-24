cd9eefec127bba5e3b5dfb267271103f5f1f7f03
farad47 <50584441+farad47@users.noreply.github.com>
2026-06-24 20:19:04 +0200

    add endpoints

--- SIMS.API/Program.cs
+++ SIMS.API/Program.cs
@@ -1,3 +1,17 @@
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using SIMS.Application;
+using SIMS.Application.Products.Commands;
+using SIMS.Application.Products.Queries;
+using SIMS.Application.Addresses.Queries;
+using SIMS.Application.Countries.Queries;
+using SIMS.Application.Customers.Queries;
+using SIMS.Infrastructure;
+using SIMS.Infrastructure.Repositories;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +22,12 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyMarker).Assembly));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter());
+    });
 
 var app = builder.Build();
 
@@ -20,6 +40,37 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPost("/products", async (CreateProductCommand command, ISender mediatr, CancellationToken ct) => 
+{
+    var result = await mediatr.Send(command, ct);
+    if (result?.Value is null || Guid.Empty == result.Value.id) return Results.BadRequest();
+    return Results.Created($"/products/{result.Value.id}", new { id = result.Value.id });
+});
+
+app.MapGet("/products", async (ISender mediatr, CancellationToken ct) =>
+{
+    var result = await mediatr.Send(new GetListProductsQuery(), ct);
+    return Results.Ok(result.Value);
+});
+
+app.MapGet("/addresses", async (ISender mediatr, CancellationToken ct) =>
+{
+    var result = await mediatr.Send(new GetListAddressesQuery(), ct);
+    return Results.Ok(result.Value);
+});
+
+app.MapGet("/countries", async (ISender mediatr, CancellationToken ct) =>
+{
+    var result = await mediatr.Send(new GetListCountriesQuery(), ct);
+    return Results.Ok(result.Value);
+});
+
+app.MapGet("/customers", async (ISender mediatr, CancellationToken ct) =>
+{
+    var result = await mediatr.Send(new GetListCustomersQuery(), ct);
+    return Results.Ok(result.Value);
+});
 
 app.MapGet("/", () =>
 {
